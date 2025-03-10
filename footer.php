912a9c9b1e3a1254596c858809c6b27dcac079f5
Subash Mohanty <42642790+Subashmohanty@users.noreply.github.com>
2025-03-10 22:46:01 +0530

    Delete footer.php

--- footer.php
+++
@@ -1,6 +0,0 @@
-    <script src="js/jquery-cdn.js"></script>
-    <script src="js/bootstrap.js"></script>
-    <script src="js/intelli-input.js"></script>
-    <script src="js/common.js"></script>
-</body>
-</html>
